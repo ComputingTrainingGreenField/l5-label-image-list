492f46db5d1353b88577ea96ac94008b998b17fc
Tong Du <tongdu@gmail.com>
2018-10-23 02:18:13 -0700

    ngFor bind array

--- src/app/browse/browse.component.ts
+++ src/app/browse/browse.component.ts
@@ -1,7 +1,4 @@
 import { Component, OnInit } from "@angular/core";
-import { TouchGestureEventData } from "tns-core-modules/ui/gestures/gestures";
-import { EventData, View } from "tns-core-modules/ui/page/page";
-import { TabViewItem } from "ui/tab-view"
 
 @Component({
     selector: "Browse",
@@ -9,16 +6,12 @@ import { TabViewItem } from "ui/tab-view"
     templateUrl: "./browse.component.html"
 })
 export class BrowseComponent implements OnInit {
-    icon_x = 50;
-    icon_y = 50;
-    text_x = 100;
-    text_y = 100;
-    label_x = 50;
-    label_y = 200;
-    count = 0; 
-    timer = null;
-    view_width = 300;
-    view_height = 500;
+    itemList = [
+        {x:100, y:100, text:' A '},
+        {x:10, y:200, text:' B '},
+        {x:200, y:300, text:' C '},
+        {x:130, y:330, text:' D '},
+    ]
 
     constructor() {
         // Use the component constructor to inject providers.
@@ -26,17 +19,6 @@ export class BrowseComponent implements OnInit {
 
     ngOnInit(): void {
         // Use the "ngOnInit" handler to initialize data for the view.
-        this.timer = setInterval(() => {
-            this.text_x = Math.floor(Math.random() * this.view_width);
-            this.text_y = Math.floor(Math.random() * this.view_height);
-        }, 1000);
     }
 
-    onTouch(event:TouchGestureEventData): void {
-        if(event.action == 'up') {
-            this.count++;
-            this.text_x = event.getX();
-            this.text_y = event.getY();    
-        }
-    }
 }
