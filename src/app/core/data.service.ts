2cba16947a34a5f20006ac530ce452cc30741598
Tong Du <tongdu@gmail.com>
2018-12-06 21:08:16 -0800

    fix item sync issue

--- src/app/core/data.service.ts
+++ src/app/core/data.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@angular/core";
-import { getJSON } from "tns-core-modules/http";
+import { getJSON, request } from "tns-core-modules/http";
 import { ObservableArray } from "tns-core-modules/data/observable-array/observable-array";
 
 export interface IDataItem {
@@ -13,10 +13,11 @@ export interface IDataItem {
 export class DataService {
 
     private items = new ObservableArray<IDataItem>();
+    private baseUrl = "http://127.0.0.1:5000/";
 
     constructor() {
         let dataService = this;
-        getJSON("http://127.0.0.1:5000/items").then((r: IDataItem[]) => {
+        getJSON(this.baseUrl + "items/").then((r: IDataItem[]) => {
             dataService.items.push(r);
         }, (e) => {
         });
@@ -28,5 +29,42 @@ export class DataService {
 
     getItem(id: number): IDataItem {
         return this.items.filter((item) => item.id === id)[0];
+    }
+
+    addItem(item: IDataItem) {
+        this.items.push(item);
+        request({
+            url: this.baseUrl + "items/",
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            content: JSON.stringify(item)
+        }).then((response) => {
+            const result = response.content.toJSON();
+        }, (e) => {
+        });
+    }
+
+    updateItem(item: IDataItem) {
+        this.items[item.id-1] = item;
+        request({
+            url: this.baseUrl + "items/" + item.id,
+            method: "PUT",
+            headers: { "Content-Type": "application/json" },
+            content: JSON.stringify(item)
+        }).then((response) => {
+            const result = response.content.toJSON();
+        }, (e) => {
+        });
+    }
+
+    deleteItem(item: IDataItem) {
+        this.items.splice(item.id - 1);
+        request({
+            url: this.baseUrl + "items/" + item.id,
+            method: "DELETE",
+        }).then((response) => {
+            const result = response.content.toJSON();
+        }, (e) => {
+        });
     }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -20,7 +20,8 @@ export class HomeComponent implements OnInit {
 
     onAdd() {
         let newId = this.items.length + 1;
-        this.items.push({id: newId, name: "Item " + newId, description: "Description for Item "+newId, src:"http://www.stleos.uq.edu.au/wp-content/uploads/2016/08/image-placeholder.png"});
+        let newItem = {id: newId, name: "Item " + newId, description: "Description for Item "+newId, src:"http://www.stleos.uq.edu.au/wp-content/uploads/2016/08/image-placeholder.png"};
+        this.itemService.addItem(newItem);
         this.router.navigate(['../item', newId], { relativeTo: this.route});
     }
 }
